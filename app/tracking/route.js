53a6bbe6bf625b45f2275b23203ad77f280405c3
hf <hufan@lingxing.com>
2023-12-20 10:05:39 +0800

    add

--- app/tracking/route.js
+++ app/tracking/route.js
@@ -1,17 +1,19 @@
 import path from 'path'
 import fs from 'fs'
+import { NextResponse } from 'next/server'
 
 export function GET(request) {
-  console.log('🚀 ~ request:', request)
-  const filePath = path.resolve('./public/test.html')
-  const tpl = fs.readFileSync(filePath, 'utf8')
-  const resp = new Response(tpl, {
-    headers: {
-      ...request.headers,
-      'content-type': 'application/liquid;charset=UTF-8',
-    },
-  })
-  
+  // console.log('🚀 ~ request:', request)
+  // const filePath = path.resolve('./public/test.html')
+  // const tpl = fs.readFileSync(filePath, 'utf8')
+  // const resp = new NextResponse(tpl, {
+  //   headers: {
+  //     'content-type': 'application/liquid;charset=UTF-8',
+  //   },
+  // })
 
-  return resp
+
+  // return resp
+
+  return NextResponse.json({ name: 'test', age: 18 })
 }
