89a80f4966ed2ad9d0898ba9437fdcb69cddfc5a
hf <hufan@lingxing.com>
2023-12-20 09:29:20 +0800

    image

--- next.config.js
+++ next.config.js
@@ -2,7 +2,15 @@ const isProd = process.env.NODE_ENV === 'production'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  assetPrefix: isProd ? 'https://test-next-rfirefly.vercel.app' : ''
+  assetPrefix: isProd ? 'https://test-next-rfirefly.vercel.app' : '',
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'test-next-rfirefly.vercel.app',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
