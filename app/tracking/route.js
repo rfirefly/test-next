aa93ce166dea4910bedfd620ee6f537cc55d524a
hf <hufan@lingxing.com>
2023-12-20 10:14:36 +0800

    track

--- app/tracking/route.js
+++ app/tracking/route.js
@@ -4,16 +4,15 @@ import { NextResponse } from 'next/server'
 
 export function GET(request) {
   console.log('🚀 ~ request:', request)
-  // const filePath = path.resolve('./public/test.html')
-  // const tpl = fs.readFileSync(filePath, 'utf8')
-  // const resp = new NextResponse(tpl, {
-  //   headers: {
-  //     'content-type': 'application/liquid;charset=UTF-8',
-  //   },
-  // })
+  const filePath = path.resolve('./public/test.html')
+  const tpl = fs.readFileSync(filePath, 'utf8')
+  const resp = new NextResponse(tpl, {
+    headers: {
+      'content-type': 'application/liquid;charset=UTF-8',
+    },
+  })
 
 
-  // return resp
+  return resp
 
-  return NextResponse.json({ name: 'test', age: 18 })
 }
