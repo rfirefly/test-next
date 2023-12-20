92db5b68783f9beaec34cb0aac142fdb1222bd35
hf <hufan@lingxing.com>
2023-12-20 09:34:57 +0800

    image

--- app/page.js
+++ app/page.js
@@ -17,7 +17,8 @@ export default function Home() {
           >
             By{' '}
             <Image
-              src="/vercel.svg"
+              src={`${process.env.ASSET_PREFIX}/vercel.svg`}
+              
               alt="Vercel Logo"
               className={styles.vercelLogo}
               width={100}
@@ -31,7 +32,7 @@ export default function Home() {
       <div className={styles.center}>
         <Image
           className={styles.logo}
-          src="/next.svg"
+          src={`${process.env.ASSET_PREFIX}/next.svg`}
           alt="Next.js Logo"
           width={180}
           height={37}
